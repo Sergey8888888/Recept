4ce71dea6c8957515af39bc5599ac6f81ef9561f
meechisdeadd <ifuckyourgrandma@mail.ru>
2023-12-15 15:00:24 +0300

    HELL o

--- components/OneRecipe.jsx
+++ components/OneRecipe.jsx
@@ -8,7 +8,7 @@ function OneRecipe({ recipe, user }) {
         <img src={recipe.image} style={{ width: '200px', height: '200px' }} />
         <h2>{recipe.name}</h2>
         <h3>Время приготовления:{recipe.timing} МИНУТ </h3>
-        <p>{recipe.resipes}</p>
+        <p>{recipe.recipes}</p>
         <p>{recipe.description}</p>
       </div>
     </Layout>
