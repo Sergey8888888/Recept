286d90b575096f97d91d590913b8b72d5dd59d04
meechisdeadd <ifuckyourgrandma@mail.ru>
2023-12-15 12:32:32 +0300

    Tri XXX

--- routes/view/main.routes.js
+++ routes/view/main.routes.js
@@ -5,14 +5,43 @@ const OneRecipe = require('../../components/OneRecipe');
 
 const { Card } = require('../../db/models');
 
+// router.get('/', async (req, res) => {
+//   try {
+//     const { user } = res.locals;
+//     const recipes = await Card.findAll();
+//     const html = await res.renderComponent(MainPage, {
+//       title: 'Main Page',
+//       recipes,
+//       user,
+//     });
+//     res.send(html);
+//   } catch ({ message }) {
+//     console.log(message);
+//   }
+// });
 router.get('/', async (req, res) => {
   try {
+    const { sort } = req.query;
     const { user } = res.locals;
-    const recipes = await Card.findAll();
+    let recipes;
+
+    if (sort === 'ASC') {
+      recipes = await Card.findAll({
+        order: [['timing', 'ASC']],
+      });
+    } else if (sort === 'DESC') {
+      recipes = await Card.findAll({
+        order: [['timing', 'DESC']],
+      });
+    } else {
+      recipes = await Card.findAll();
+    }
+
     const html = await res.renderComponent(MainPage, {
       title: 'Main Page',
       recipes,
       user,
+      selectedSort: sort,
     });
     res.send(html);
   } catch ({ message }) {
